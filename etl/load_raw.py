0187197b9d51b514d365ce405a0c8bf5328686b6
Kealiiahonui Ohumukini <kealii.ohumukini@gmail.com>
2026-04-23 22:48:20 -0700

    Implemented ETL script #1 - Raw Loading

--- etl/load_raw.py
+++ etl/load_raw.py
@@ -5,3 +5,25 @@ import os
 from dotenv import load_dotenv
 from psycopg2.extras import Json
 from datetime import datetime, UTC
+
+DB_CONN = os.getenv("DB_CONN")
+
+def load_raw(json_path): 
+    with psycopg2.connect(DB_CONN) as conn: 
+        with conn.cursor() as cursor: 
+            with open(json_path) as file_in: 
+                data = json.load(file_in)
+
+            rows_to_insert = [
+                (datetime.fromisoformat(item["scraped_at"]), Json(item))
+                for item in data
+            ] 
+
+            from psycopg2.extras import execute_batch
+            execute_batch(cursor, """
+                INSERT INTO raw_hn (scraped_at, payload)
+                VALUES (%s, %s)              
+            """, rows_to_insert)   
+
+if __name__ == "__main__": 
+    load_raw("hn_raw.json")
