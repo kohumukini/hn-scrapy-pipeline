c02247b23bbbdf71443e881d493e8ffed93048a7
Kealiiahonui Ohumukini <kealii.ohumukini@gmail.com>
2026-04-23 23:07:24 -0700

    Created database access and added gitignore

--- etl/load_raw.py
+++ etl/load_raw.py
@@ -6,10 +6,16 @@ from dotenv import load_dotenv
 from psycopg2.extras import Json
 from datetime import datetime, UTC
 
-DB_CONN = os.getenv("DB_CONN")
+load_dotenv()
+
+dbname = os.getenv("DB_NAME")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
 
 def load_raw(json_path): 
-    with psycopg2.connect(DB_CONN) as conn: 
+    with psycopg2.connect(conn) as conn: 
         with conn.cursor() as cursor: 
             with open(json_path) as file_in: 
                 data = json.load(file_in)
@@ -26,4 +32,4 @@ def load_raw(json_path):
             """, rows_to_insert)   
 
 if __name__ == "__main__": 
-    load_raw("hn_raw.json")
+    load_raw("scraping/hn_scraper/hn_raw.json")
